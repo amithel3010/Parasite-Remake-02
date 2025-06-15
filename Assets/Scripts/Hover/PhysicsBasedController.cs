960e0e09f7b27b6347af75100c523084785ee650
Amit <amithel3010@gmail.com>
2025-06-15 17:44:36 +0300

    Added Brute Prefab

--- Assets/Scripts/Hover/PhysicsBasedController.cs
+++ Assets/Scripts/Hover/PhysicsBasedController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PhysicsBasedController : MonoBehaviour
 {
     //Keeps Gameobject hovering at a certain ride height using a dampened spring

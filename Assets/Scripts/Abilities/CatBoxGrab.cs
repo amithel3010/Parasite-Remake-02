9de50c4f4cc99a14c7482b62419f13f59b7860b4
Amit <amithel3010@gmail.com>
2025-07-10 12:41:03 +0300

    Started working on cat's ability to grab boxes

---
+++ Assets/Scripts/Abilities/CatBoxGrab.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CatBoxGrab : MonoBehaviour
+{
+    //On action press, if the cat is near a wooden box,
+    //snap the box to a holdingPos,
+    //adjust ride height and ground ray distance?
+    //box becomes kinematic?
+    //cat should feel heavier when holding box. adjust rb.mass?
+    // if already holding box, release it.
+
+}

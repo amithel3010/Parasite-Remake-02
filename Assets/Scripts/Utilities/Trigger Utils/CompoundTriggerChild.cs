a08fb52addaa50aeb968c0e03337a47f98e40d4c
Amit <amithel3010@gmail.com>
2025-07-21 10:02:45 +0300

    Updated Prefabs

--- Assets/Scripts/Utilities/Trigger Utils/CompoundTriggerChild.cs
+++ Assets/Scripts/Utilities/Trigger Utils/CompoundTriggerChild.cs
@@ -4,26 +4,44 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class CompoundTriggerChild : MonoBehaviour
 {
-    //TODO: make this more generic
+    //TODO: Add Debug Visuals
 
     private CompoundTrigger _compoundTrigger;
     private Collider _thisTrigger;
+
+    private bool _shouldDrawDebugGfx;
 
     private void Awake()
     {
         _compoundTrigger = GetComponentInParent<CompoundTrigger>();
         _thisTrigger = GetComponent<Collider>();
+
+        if (_thisTrigger.isTrigger == false)
+        {
+            Debug.LogWarning(this + "Is on a collider that isn't a trigger. setting it as trigger.");
+            _thisTrigger.isTrigger = true;
+        }
+
+        if (_compoundTrigger == null)
+        {
+            Debug.LogError(this + "Doesn't have a parent with a compound trigger script!");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //if (other.gameObject.layer == LayerUtils.PlayerControlledLayer)
-            _compoundTrigger.ObjectEntered(_thisTrigger, other);
+        _compoundTrigger.ObjectEntered(_thisTrigger, other);
     }
 
     private void OnTriggerExit(Collider other)
     {
         //if (other.gameObject.layer == LayerUtils.PlayerControlledLayer)
-            _compoundTrigger.ObjectExited(_thisTrigger, other);
+        _compoundTrigger.ObjectExited(_thisTrigger, other);
+    }
+
+    public void SetDebugGfxActive(bool active)
+    {
+        _shouldDrawDebugGfx = active;
     }
 }

4d4a6bbe1c9cfb0f8b73db2c15d933469abaa27e
Amit <amithel3010@gmail.com>
2025-08-04 20:02:16 +0300

    Added DoTween and started playing around with it

--- Assets/Scripts/UI/FillUi.cs
+++ Assets/Scripts/UI/FillUi.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using Mono.Cecil;
 using UnityEngine;
 
@@ -56,16 +57,20 @@ public class FillUi : MonoBehaviour, IPossessionSensitive
         float percent = Mathf.Clamp01(current / max);
         float fillAngle = (1f - percent) * 360f; // invert the fill
 
-        _fillMaterial.SetFloat(_fillPropertyName, fillAngle);
+        //_fillMaterial.SetFloat(_fillPropertyName, fillAngle);
+        _fillMaterial.DOFloat(fillAngle, _fillPropertyName, 0.8f).SetEase(Ease.InOutCubic);
     }
 
     public void OnPossessed(Parasite playerParasite, IInputSource inputSource)
     {
         _staminaBar.enabled = true;
+        _staminaBar.transform.DOScale(0, 0);
+        _staminaBar.transform.DOScale(0.7f, 2f).SetEase(Ease.InOutCubic); // breakable
     }
 
     public void OnUnPossessed(Parasite playerParasite)
     {
+        _staminaBar.transform.DOScale(0f, 2f).SetEase(Ease.InOutCubic);
         _staminaBar.enabled = false;
     }
 }

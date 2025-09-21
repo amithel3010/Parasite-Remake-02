f811681c4c214dd961f981271eedd1ab15091a8f
Amit <amithel3010@gmail.com>
2025-09-21 19:51:49 +0300

    Tried Adding Restart but it doesn't work yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -45,7 +46,7 @@ public class GameManager : MonoBehaviour
         _isPaused = true;
     }
 
-    public void Restart()
+    public void RestartFromCheckpoint()
     {
         Time.timeScale = 1f;
         CheckpointManager.Instance.RespawnParasite();
@@ -53,5 +54,10 @@ public class GameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         _isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
